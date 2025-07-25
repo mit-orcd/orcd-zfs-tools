518a8f138e16006f934f6c4b137e5ba08f7db70b
Dariusz Bielik <bdarek@gmail.com>
2025-07-24 20:45:22 -0400

    Update ensures the script prints the error message and exits gracefully when the group is not found

--- zfs-make-share-pool.sh
+++ zfs-make-share-pool.sh
@@ -52,7 +52,7 @@ fi
 echo "Will attempt to create shared pool on server ${DZSRV}"
 
 # Lookup shared group GID in ldap.mit.edu
-GID=$(ldapsearch -LLL -x -h ldap.mit.edu -b "ou=lists,ou=moira,dc=mit,dc=edu" "cn=orcd_rg_shared_pi_${USR}" gidNumber | grep '^gidNumber' | awk '{print $2}')
+GID=$(ldapsearch -LLL -x -h ldap.mit.edu -b "ou=lists,ou=moira,dc=mit,dc=edu" "cn=orcd_rg_shared_pi_${USR}" gidNumber | grep '^gidNumber' | awk '{print $2}' 2>/dev/null || true)
 
 if [[ -z "$GID" ]]; then
     echo -e "${RED} Exiting - shared group orcd_rg_shared_pi_${USR} doesn't exist in ldap.mit.edu ${NCL}"
