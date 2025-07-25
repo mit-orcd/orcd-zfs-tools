23e4f0bd2aee956dc5e52787708a600aadedcafe
Dariusz Bielik <bdarek@gmail.com>
2025-07-24 23:03:49 -0400

    shelchecks complete

--- zfs-make-share-pool.sh
+++ zfs-make-share-pool.sh
@@ -59,17 +59,17 @@ if [[ -z "$GID" ]]; then
     exit 1
 fi
 
-GROUP="orcd_rg_shared_pi_${USR}"
+GRP=orcd_rg_shared_pi_${USR}
 
 # Check if group is in moira (local LDAP)
-if ! getent group "$GROUP" &>/dev/null; then
+if ! getent group "$GRP" &>/dev/null; then
     echo -e "${GRN} Shared group $GROUP not yet added to moira, attempting to add please wait ... ${NCL}"
     for LDP in "${LDAPS[@]}"; do
-        ssh "$SESRV" ssh "$LDP" /root/ldap-by-hand/add-group-moira "$GROUP"
-        ssh "$SESRV" ssh "$LDP" /root/ldap-by-hand/add-user-to-group-moira "$GROUP"
+        ssh "$SESRV" ssh "$LDP" /root/ldap-by-hand/add-group-moira orcd_rg_shared_pi_"${USR}"
+        ssh "$SESRV" ssh "$LDP" /root/ldap-by-hand/add-user-to-group-moira orcd_rg_shared_pi_"${USR}" 
     done
     echo "Validating the group is now visible..."
-    MGD=$(getent group "$GROUP")
+    MGD=$(getent group "$GRP")
     if [[ "$MGD" =~ ^.+/$ ]]; then
         echo -e "${RED} Exiting - there was an issue adding the group - please add manually and re-run this script ${NCL}"
         exit 1
@@ -77,7 +77,7 @@ if ! getent group "$GROUP" &>/dev/null; then
         echo -e "${GRN} Shared group has been added - $MGD ${NCL}"
     fi
 else
-    echo -e "${GRN} Great - group $GROUP already in moira - proceeding ${NCL}"
+    echo -e "${GRN} Great - group $GRP already in moira - proceeding ${NCL}"
 fi
 
 echo -e "${YLO} - passed the checks"
@@ -88,9 +88,9 @@ POOL=$(ssh "$DZSRV" zfs list | grep "pool " | awk '{print $5}')
 ZFSP=$(echo "$POOL" | awk -F "/" '{print $2"/"$3}')
 
 # Create the shared dataset
-ssh "$DZSRV" zfs create "${ZFSP}/${USR}_shared"
-ssh "$DZSRV" zfs set quota=5T "${ZFSP}/${USR}_shared"
-ssh "$DZSRV" chmod 2770 "${POOL}/${USR}_shared"
-ssh "$DZSRV" chown "root:${GROUP}" "${POOL}/${USR}_shared"
+ssh "$DZSRV" zfs create "${ZFSP}"/"${USR}"_shared
+ssh "$DZSRV" zfs set quota=5T "${ZFSP}"/"${USR}"_shared
+ssh "$DZSRV" chmod 2770 "${POOL}"/"${USR}"_shared
+ssh "$DZSRV" chown "root:${GRP}" "${POOL}"/"${USR}"_shared
 
-echo "New shared pool storage ${POOL}/${USR}_shared for PI ${USR} has been created on ${DZSRV} -- owner [root]:[${GROUP}]"
+echo "New shared pool storage ${POOL}/${USR}_shared for PI ${USR} has been created on ${DZSRV} -- owner [root]:[${GRP}]"
